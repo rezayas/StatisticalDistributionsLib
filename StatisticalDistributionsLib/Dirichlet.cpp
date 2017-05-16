53a6b4e106ebde84618b3d5309f64b0542ae0879
Eyal Minsky-Fenick <eyjmfc@gmail.com>
2017-05-16 13:54:22 -0400

    Added Dirichlet.

---
+++ StatisticalDistributionsLib/Dirichlet.cpp
@@ -0,0 +1,37 @@
+#include "Dirichlet.h"
+
+namespace StatisticalDistributions {
+
+  template<size_t N>
+  Dirichlet::Dirichlet(std::array<long double, N> alphas)
+    : beta(-1), alphas(alphas) {
+    for(int i = 0; i < N; i++)
+      dists[i] = std::gamma_distribution<long double>(alphas[i]);
+  }
+  template<size_t N>
+  long double Dirichlet::pdf(std::array<long double, N> x) {
+    if(beta == -1) {
+      long double asum = 0;
+      for(int i = 0; i < N; i++) {
+	beta *= std::tgamma(alphas[i]);
+	asum += alphas[i];
+      }
+      beta /= std::tgamma(asum);
+    }
+    long double ans = 1;
+    for(int i = 0; i < N; i++)
+      ans *= std::pow(x[i], alphas[i] - 1);
+    return(ans);
+  }
+  template<size_t N>
+  long double Dirichlet::operator()(std::mt19937_64 &g) {
+    std::array<long double, N> rval;
+    long double tot = 0;
+    for(int i = 0; i < N; i++)
+      tot += rval[i] = dists[i](g);
+    for(int i = 0; i < N; i++)
+      rval[i] /= tot;
+    return(rval);
+  }
+}
+
---
+++ StatisticalDistributionsLib/Dirichlet.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "StatisticalDistribution.h"
+#include <array>
+
+namespace StatisticalDistributions {
+  using namespace std;
+  template<size_t N>
+  class Dirichlet : public StatisticalDistribution<array<long double, N>> {
+  public:
+    Dirichlet(array<long double, N> alphas);
+    virtual long double pdf(array<long double, N> x);
+    virtual long double cdf(array<long double, N> x) {
+      return(0); // CDF unimplementable.
+    }
+    virtual array<long double, N> Inverse(long double x) {
+      return(0); // iCDF unimplementable.
+    }
+    virtual array<long double, N> operator()(mt19937_64 &g);
+  private:
+    long double beta;
+    const array<long double, N> alphas;
+    array<gamma_distribution<long double>, N> dists;
+  };
+}
