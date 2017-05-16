a2092f521690472db34c145bfaa4d06b7ded1fe2
Eyal Minsky-Fenick <eyjmfc@gmail.com>
2017-05-16 11:06:08 -0400

    Added beta-distribution support.

---
+++ StatisticalDistributionsLib/Beta.cpp
@@ -0,0 +1,27 @@
+#include "Beta.h"
+#include <boost/math/special_functions/beta.hpp>
+
+namespace StatisticalDistributions
+{
+  Beta::Beta(long double alpha, long double beta)
+    : alpha(alpha), beta(beta), dist(alpha, beta) {}
+
+  Beta Beta::mean_sdev(long double mu, long double sigma) {
+    Beta x(boost::math::beta_distribution<long double>::find_alpha(mu, sigma),
+	   boost::math::beta_distribution<long double>::find_beta(mu, sigma));
+    return(x);
+  }
+  
+  long double Beta::pdf(long double value) {
+    return(boost::math::pdf(dist, value));
+  }
+  long double Beta::cdf(long double value) {
+    return(boost::math::cdf(dist, value));
+  }
+  long double Beta::Inverse(long double value) {
+    return(boost::math::quantile(dist, value));
+  }
+  long double Beta::operator()(std::mt19937_64 &g) {
+    return(Inverse(uni(g)));
+  }
+}
---
+++ StatisticalDistributionsLib/Beta.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "StatisticalDistribution.h"
+#include <boost/math/distributions/beta.hpp>
+
+namespace StatisticalDistributions
+{
+  class Beta : public StatisticalDistribution<long double> {
+  public:
+    Beta(long double alpha, long double beta);
+    static Beta mean_sdev(long double mu, long double sigma);
+    virtual long double pdf(long double value);
+    virtual long double cdf(long double value);
+    virtual long double Inverse(long double value);
+    virtual long double Sample(RNG &rng);
+    virtual long double operator()(std::mt19937_64 &g);
+  private:
+    const long double alpha, beta;
+    static std::uniform_real_distribution<long double> uni;
+    const boost::math::beta_distribution<long double> dist;
+  };
+}
