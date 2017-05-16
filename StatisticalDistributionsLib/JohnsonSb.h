96bab4b85546485055a1d4a8441ddf272e507ad1
Eyal Minsky-Fenick <eyjmfc@gmail.com>
2017-05-16 12:05:02 -0400

    Added Johnson distributions.

---
+++ StatisticalDistributionsLib/JohnsonSb.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "StatisticalDistribution.h"
+
+namespace StatisticalDistributions
+{
+  class JohnsonSb : public StatisticalDistribution<long double> {
+  public:
+    JohnsonSb(long double gamma, long double xi,
+	      long double delta, long double lambda);
+    virtual long double pdf(long double value);
+    virtual long double cdf(long double value);
+    virtual long double Inverse(long double value);
+    virtual long double Sample(RNG &rng);
+    virtual long double operator()(std::mt19937_64 &g);
+  private:
+    const long double gamma, xi, delta, lambda;
+    std::normal_distribution<long double> dist;
+  };
+}
---
+++ StatisticalDistributionsLib/JohnsonSu.cpp
@@ -0,0 +1,29 @@
+#include "JohnsonSu.h"
+#include <boost/math/special_functions/erf.hpp>
+#define SQUARE(x) ((x)*(x))
+
+namespace StatisticalDistributions
+{
+  static const long double sqrt2 = sqrt(2);
+  using namespace std;
+  JohnsonSu::JohnsonSu(long double gamma, long double xi, long double delta, long double lambda)
+    : dist(-gamma / delta, delta), gamma(gamma), xi(xi), delta(delta),
+      lambda(lambda) {}
+
+  long double JohnsonSu::pdf(long double value) {
+    static const long double irt2pi = 1 / sqrt(8 * atan(1.0L));
+    return(delta * irt2pi / lambda / sqrt(1 + SQUARE((value - sigma)/lambda))
+	   * exp(-SQUARE(gamma + delta * asinh((value - xi) / lambda))/2));
+  }
+  long double JohnsonSu::cdf(long double value) {
+    return((erf((gamma + delta * asinh((value - xi)/lambda))/sqrt(2))
+	    + 1) / 2);
+  }
+  long double JohnsonSu::Inverse(long double value) {
+    return(sinh((boost::math::erf_inv(2 * value - 1)*sqrt(2) - gamma)/delta)
+	   * lambda + xi);
+  }
+  long double JohnsonSu::operator()(std::mt19937_64 &g) {
+    return(sinh(this->dist(g)) * lambda + xi);
+  }
+}
