035d9c5da34f58edf64e7026d9990b3971d4f51d
Eyal Minsky-Fenick <eyjmfc@gmail.com>
2017-06-15 17:04:29 -0400

    Changed Bernoulli to ask for and get longs.

--- StatisticalDistributionsLib/Bernoulli.cpp
+++ StatisticalDistributionsLib/Bernoulli.cpp
@@ -4,16 +4,16 @@ namespace StatisticalDistributions {
 
   Bernoulli::Bernoulli(long double p)
     : dist(p), cdist(p) {}
-  long double Bernoulli::pdf(bool value) const {
+  long double Bernoulli::pdf(long value) const {
     return(boost::math::pdf(cdist, value));
   }
-  long double Bernoulli::cdf(bool value) const {
+  long double Bernoulli::cdf(long value) const {
     return(boost::math::cdf(cdist, value));
   }
-  bool Bernoulli::Inverse(long double value) const {
+  long Bernoulli::Inverse(long double value) const {
     return((bool)boost::math::quantile(cdist, value));
   }
-  bool Bernoulli::operator()(std::mt19937_64 &g) const {
+  long Bernoulli::operator()(std::mt19937_64 &g) const {
     return(this->dist(g));
   }
 }
--- StatisticalDistributionsLib/Bernoulli.h
+++ StatisticalDistributionsLib/Bernoulli.h
@@ -3,14 +3,14 @@
 #include <boost/math/distributions/bernoulli.hpp>
 
 namespace StatisticalDistributions {
-  class Bernoulli : public StatisticalDistribution<bool> {
+  class Bernoulli : public StatisticalDistribution<long> {
   public:
     Bernoulli(long double p = .5);
     // p is the probability of success.
-    virtual long double pdf(bool value) const;
-    virtual long double cdf(bool value) const;
-    virtual bool Inverse(long double value) const;
-    virtual bool operator()(std::mt19937_64 &g) const ;
+    virtual long double pdf(long value) const;
+    virtual long double cdf(long value) const;
+    virtual long Inverse(long double value) const;
+    virtual long operator()(std::mt19937_64 &g) const ;
     const boost::math::bernoulli_distribution<long double, POLROUNDDOWN> cdist;
   private:
     mutable std::bernoulli_distribution dist;
